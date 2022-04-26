f4e7d534f3667cd131e8d5c04e6167251ddb1bcd
Tyler Swindell <tylerswindell@Tylers-MacBook-Pro-2.local>
2022-04-25 23:06:41 -0400

    added ability to add task, delete task, and toggle reminders.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from 'src/app/services/task.service';
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
 @Component({
@@ -18,5 +18,22 @@ export class TasksComponent implements OnInit {
     // If subscribe isn't chained then nothing will load 
     // because it will not update the data
     this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+  }
+
+  // Requires a service method to delete
+  deleteTask(task: Task) {
+    this.taskService.deleteTasks(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    });
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminders(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTasks(task).subscribe();
+    this.tasks.push(task);
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import {Task} from '../Task';
-import {TASKS} from '../mock-tasks';
-import { TasksComponent } from '../components/tasks/tasks.component';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +21,18 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
   } 
+
+  deleteTasks(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminders(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTasks(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
 }
